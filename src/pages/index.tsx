8d782fa2ebd14f346b1475115603838bb9e7f47d
Pedro Duarte <pedrobduarte2000@gmail.com>
2024-01-31 19:45:04 +0000

    navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,24 @@ import { CardComponent } from "../components/postCard";
 import useRootPage from "../hooks/useRootPage";
 
 function RootPage() {
-  const { postsList, handleCardClick } = useRootPage();
+  const { postsList, handleCardClick, selectValue, handleSelect } =
+    useRootPage();
 
   return (
     <Container>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginBottom: "2rem",
+        }}
+      >
+        <select value={selectValue} onChange={handleSelect}>
+          <option value="">Filtros</option>
+          <option value="newest">Mais recente</option>
+          <option value="oldest">Mais antiga</option>
+        </select>
+      </div>
       <Row lg="4">
         {postsList.map((card) => (
           <Col key={card.id}>
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,17 +1,52 @@
-import { Container } from "reactstrap";
-import { PostDetails } from "../../components/postDetails";
+import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { useState } from "react";
 
-function PostPage() {
+function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleCreatePost = () => {
+    // Add logic to send data to the server or save it in a database
+    // For simplicity, we'll just log the data to the console
+    const newPost = { title, imageUrl, description };
+    console.log("New Post:", newPost);
+  };
+
   return (
     <Container>
-      <PostDetails
-        imgUrl="https://picsum.photos/300/200"
-        title="Card title"
-        description="Some quick example text to build on the card title and make up the
-        bulk of the card‘s content."
-      />
+      <Form>
+        <FormGroup>
+          <Label for="title">Title</Label>
+          <Input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="imageUrl">Image URL</Label>
+          <Input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="description">Description</Label>
+          <Input
+            type="textarea"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </FormGroup>
+        <Button onClick={handleCreatePost}>Create Post</Button>
+      </Form>
     </Container>
   );
 }
 
-export default PostPage;
+export default CreatePost;
